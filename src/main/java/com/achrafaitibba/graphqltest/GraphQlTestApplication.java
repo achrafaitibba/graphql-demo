1e58ba721bcf91ad949164f33c02eff4bcd5a876
achrafaitibba <aitibbaachraf@gmail.com>
2024-01-07 18:24:34 +0100

    ;

--- src/main/java/com/achrafaitibba/graphqltest/GraphQlTestApplication.java
+++ src/main/java/com/achrafaitibba/graphqltest/GraphQlTestApplication.java
@@ -2,10 +2,10 @@ package com.achrafaitibba.graphqltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class GraphQlTestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GraphQlTestApplication.class, args);
     }
